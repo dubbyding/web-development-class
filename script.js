5d256dd2ed059d563385577e1c0883c6be6d2ff5
Raj Maharjan <rmaharjan.rm90@gmail.com>
2025-06-08 13:26:00 +0545

    feat: basics of html, css and js

---
+++ script.js
@@ -0,0 +1,27 @@
+/**
+ * Hoisting (Interview Question)
+ *
+ * let
+ * var
+ * const Reassign garna mildaina
+ *
+ * Kei assign xaina vane undefined basxa
+ */
+console.log("Hello");
+
+/**
+ * function
+ * arrow function
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+const diff = (a, b) => a - b;
+
+const a = [1, 2, 3, 4];
+
+console.log(a);
+a.push(5);
+console.log(a);
